0462a8a3aafe845c49c47d6c4b620634ec423e3f
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-02-18 23:38:47 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (62.53%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    // by using sentinel node which is dummy node
+    let sentinel=new ListNode();
+    sentinel.next=head;
+
+    let prev=sentinel;
+    while(prev!=null && prev.next!=null){
+        if(prev.next.val==val){
+            prev.next=prev.next.next;
+        }
+        else{
+            prev=prev.next;
+        }
+
+    }
+     return sentinel.next;
+};
