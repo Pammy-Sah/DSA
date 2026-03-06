19b33fd71cf4c6473d5207c3ab899f436814df13
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-03-06 23:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (98.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -2,21 +2,24 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    let n=s.length-1;
-    while(n>=0){
-        if(s[n]!=" "){
-            break;
-        }
-        --n;
-    }
-    let count=0;
-    while(n>=0){
-        if(s[n]==" "){
-            break;
-        }
-        --n;
-        ++count;
-    }
-    return count;
+// var lengthOfLastWord = function(s) {
+//   let n=s.length-1;
+//   let count=0;
+//   while(n>=0){
+//     if(s[n]!=" "){
+//         ++count;
+//     }
+//     else if(count>0){
+//         break;
+//     }
+//     --n;
+//   }
+//   return count;
+// };
+
+// approach 2..
+var lengthOfLastWord=function(s){
+    s=s.trim();
+    s=s.split(" ");
+    return s[s.length-1].length;
 };
