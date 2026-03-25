c12f3ed2ec0986ece0eb4e5ec3d32fb241161f17
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-03-25 22:58:12 +0530

    Time: 1 ms (32.28%) | Memory: 53.1 MB (94.88%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let L=0;
+    let R=0;
+    let count=0;
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="R"){
+            ++R;
+        }else{
+            ++L;
+        }
+        if(R==L){
+            ++count;
+            L=0;
+            R=0;
+            
+        }
+    }
+    return count;
+};
