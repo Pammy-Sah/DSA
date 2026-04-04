ef3176def96b3c2ff2fee572c744ad85fc460cf0
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-04-04 23:37:47 +0530

    Time: 26 ms (13.99%) | Memory: 60.4 MB (17.25%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(arr) {
+    let stack=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]=="+" || arr[i]=="-" || arr[i]=="*" || arr[i]=="/"){
+            let a=stack.pop();
+            let b= stack.pop();
+            let ans=eval(`${b}  ${arr[i]}  ${a}`);
+            stack.push(Math.trunc(ans));
+        }
+        else{
+            stack.push(arr[i]);
+        }
+    }
+    return Number(stack.pop());
+};
