0a71a98e26c8b4f4daa3c70f1cf5dc84769a39b3
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-04-11 21:32:26 +0530

    Time: 2 ms (96.29%) | Memory: 57.1 MB (80.99%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    let m=grid.length;
+    let n=grid[0].length;
+
+    let queue=[];
+    // first add all the rotten oranges in queue
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(grid[i][j]==2){
+                queue.push([i,j,0]);
+            }
+        }
+    }
+
+    let maxMinutes=0;
+    // mark adj nodes as rotten nd push in queue till its not empty
+
+    while(queue.length){
+        let [x,y,level]=queue.shift();
+        if(x>0 && grid[x-1][y]==1){
+            grid[x-1][y]=2;
+            queue.push([x-1,y,level+1]);
+        }
+         if(x<m-1 && grid[x+1][y]==1){
+            grid[x+1][y]=2;
+            queue.push([x+1,y,level+1]);
+        }
+         if(y<n-1 && grid[x][y+1]==1){
+            grid[x][y+1]=2;
+            queue.push([x,y+1,level+1]);
+        }
+        if(y>0 && grid[x][y-1]==1){
+            grid[x][y-1]=2;
+            queue.push([x,y-1,level+1]);
+        }
+        maxMinutes=Math.max(level,maxMinutes)
+    }
+    // finally run over each element nd check if any fresh is remaining
+      for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(grid[i][j]==1){
+               return -1
+            }
+        }
+    }
+    return maxMinutes;
+};
