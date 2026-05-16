4467c281aaf895daa861129c000a8a461f9d21ed
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-05-16 23:57:34 +0530

    Time: 49 ms (71.38%) | Memory: 55.7 MB (70.69%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.js
@@ -0,0 +1,55 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function(s1, s2, s3) {
+
+    let m = s1.length;
+    let n = s2.length;
+
+    // Length mismatch
+    if (m + n !== s3.length) {
+        return false;
+    }
+
+    // DP table
+    let dp = Array.from({ length: m + 1 }, () =>
+        Array(n + 1).fill(false)
+    );
+
+    dp[0][0] = true;
+
+    // Fill first column
+    for (let i = 1; i <= m; i++) {
+        dp[i][0] =
+            dp[i - 1][0] &&
+            s1[i - 1] === s3[i - 1];
+    }
+
+    // Fill first row
+    for (let j = 1; j <= n; j++) {
+        dp[0][j] =
+            dp[0][j - 1] &&
+            s2[j - 1] === s3[j - 1];
+    }
+
+    // Fill remaining table
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+
+            let fromS1 =
+                dp[i - 1][j] &&
+                s1[i - 1] === s3[i + j - 1];
+
+            let fromS2 =
+                dp[i][j - 1] &&
+                s2[j - 1] === s3[i + j - 1];
+
+            dp[i][j] = fromS1 || fromS2;
+        }
+    }
+
+    return dp[m][n];
+};
