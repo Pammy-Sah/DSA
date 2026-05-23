be6b64d5c4eb284b485cad25218107f4960fb549
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-05-23 22:38:31 +0530

    Time: 5 ms (43.88%) | Memory: 60.9 MB (26.07%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let firstRowZero = false;
+    let firstColZero = false;
+
+    // Check first row
+    for (let j = 0; j < cols; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowZero = true;
+        }
+    }
+
+    // Check first column
+    for (let i = 0; i < rows; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+        }
+    }
+
+    // Mark rows & cols
+    for (let i = 1; i < rows; i++) {
+        for (let j = 1; j < cols; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Set zeroes based on marks
+    for (let i = 1; i < rows; i++) {
+        for (let j = 1; j < cols; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Handle first row
+    if (firstRowZero) {
+        for (let j = 0; j < cols; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Handle first column
+    if (firstColZero) {
+        for (let i = 0; i < rows; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}
