22b78f7876528cb750021027bd10227a424f56e6
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-06-29 00:14:19 +0530

    Time: 7 ms (43.00%) | Memory: 58.2 MB (5.32%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,16 +3,30 @@
  * @param {number} target
  * @return {number[]}
  */
-function twoSum(nums, target) {
-    let map = new Map(); // stores value -> index
 
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+//  Brute force approach..
+var twoSum = function(arr, target) {
+    let n=arr.length;
+    for(let i=0;i<n-1;i++){
+        for(let j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                return [i,j];
+            }
         }
+    }
+};
 
-        map.set(nums[i], i);
+// optimal solution..
+var twoSum = function(arr, target){
+    let n=arr.length;
+    let map={};
+    for(let i=0;i<n;i++){
+        map[arr[i]]=i;
+    }
+    for(let i=0;i<n;i++){
+        let pairToFind=target-arr[i];
+        if(map[pairToFind] && map[pairToFind]!=i){
+            return [i,map[pairToFind]];
+        }
     }
 }
