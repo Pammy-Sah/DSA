f7dd1d36fed40fd457219bef214668011eb2ddfa
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-06-29 00:32:46 +0530

    Time: 2 ms (62.36%) | Memory: 55.8 MB (54.65%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(arr, target) {
+    let n=arr.length;
+    let i=0;
+    let j=n-1;
+    while(i<j){
+        let sum=arr[i]+arr[j];
+        if(sum>target){
+            --j;
+        }
+        else if(sum<target){
+            ++i;
+        }
+        else{
+            return [i+1,j+1];
+        }
+
+        
+    }
+};
