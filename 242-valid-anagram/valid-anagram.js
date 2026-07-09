36da779bc365c684a82dcb6e43868099013a401a
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-07-09 08:25:10 +0530

    Time: 12 ms (71.04%) | Memory: 54.7 MB (93.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,6 +3,30 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-      return s.split("").sort().join("")==t.split("").sort().join(""); 
-};
+//  first approach
+// var isAnagram = function(s, t) {
+//       return s.split("").sort().join("")==t.split("").sort().join(""); 
+// };
+
+// 2nd approach using hash map
+var isAnagram = function(s,t){
+    if(s.length!=t.length) return false;
+    let map={};
+
+    for(let i=0;i<s.length;i++){
+        if(!map[s[i]]){
+            map[s[i]]=1;
+        }else{
+            ++map[s[i]];
+        }
+    }
+
+    for(let i=0;i<t.length;i++){
+        if(!map[t[i]] || map[t[i]]<0){
+            return false;
+        }else{
+            --map[t[i]];
+        }
+    }
+    return true
+}
