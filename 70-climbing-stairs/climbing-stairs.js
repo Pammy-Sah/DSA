b902a2a13515d0c0c0342e537bcc24b178187510
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-09-13 13:32:24 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (49.71%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ let store = {};
+var climbStairs = function(n) {
+    if(n<=2) return n;
+    if(!store[n]){
+        store[n] = climbStairs(n-1)+ climbStairs(n-2);
+    }
+    return store[n];
+};
