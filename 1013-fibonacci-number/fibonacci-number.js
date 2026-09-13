e32ee73440b5f80aca934ca76282527131a727f6
Pammy-Sah <pammykumari.bth05@gmail.com>
2026-09-13 12:40:58 +0530

    Time: 48 ms (61.36%) | Memory: 54.2 MB (19.15%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,9 +2,25 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    if(n<=1){
-        return n;
-    }
-    return fib(n-1) + fib(n-2);
-};
+//  1. approach...
+//  top down approach (memoziation)
+
+//  let dp = { };
+// var fib = function(n) {
+//     if(n<=1){
+//         return n;
+//     }
+//     if(!dp[n]){
+//         dp[n] = fib(n-1)+fib(n-2);
+//     }
+//     return dp[n];
+// };
+
+// 2. bottom up approach  (tabulation)
+let dp = [0,1];
+var fib = function(n){
+for(let i=2;i<=n;i++){
+    dp[i]= dp[i-1]+dp[i-2];
+}
+return dp[n];
+}
